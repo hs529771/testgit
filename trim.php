39713fb02b89d244ac66ac305d725b23712db96c
Harjeet <hs529771@gmail.com>
2017-07-05 14:26:08 +0530

    php file

---
+++ trim.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+<link rel="stylesheet" type="text/css" href="a.css">
+
+<?php
+$str = "Welcome Dharamshala";
+echo $str . "<br>";
+echo trim($str,"Weal");
+?>
+</body>
+</html>
